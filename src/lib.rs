8316cd6f6440a536589ddf21a5c6adf9b7680e7e
mrtnvgr <martynovegorOF@yandex.ru>
2025-10-23 19:12:57 +0700

    feat: avoid depending on nightly, use new rust features, avoid unnecessary String copying

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,29 @@
-use std::panic::PanicInfo;
+use std::panic::{set_hook, PanicHookInfo};
 
 pub fn set_panic_hook() {
     if cfg!(not(debug_assertions)) {
-        std::panic::set_hook(Box::new(panic_hook));
+        set_hook(Box::new(panic_hook));
     }
 }
 
-fn panic_hook(info: &PanicInfo) {
-    #[cfg(feature = "nightly")]
-    let message = panic_info.message().map(|m| format!("{}", m));
+#[cfg(feature = "panic_payload_as_str")]
+fn get_panic_message<'a>(info: &'a PanicHookInfo) -> Option<&'a str> {
+    info.payload_as_str()
+}
 
-    #[cfg(not(feature = "nightly"))]
-    let message = match (
+#[cfg(not(feature = "panic_payload_as_str"))]
+fn get_panic_message<'a>(info: &'a PanicHookInfo) -> Option<&'a str> {
+    match (
         info.payload().downcast_ref::<&str>(),
         info.payload().downcast_ref::<String>(),
     ) {
-        (Some(s), _) => Some((*s).to_owned()),
-        (_, Some(s)) => Some(s.to_string()),
+        (Some(s), _) => Some(*s),
+        (_, Some(s)) => Some(s),
         (None, None) => None,
-    };
+    }
+}
 
-    message.map_or_else(
-        || log::error!("{}", info),
-        |message| log::error!("{}", message),
-    );
+fn panic_hook(info: &PanicHookInfo) {
+    let message = get_panic_message(info);
+    message.map_or_else(|| log::error!("{info}"), |message| log::error!("{message}"));
 }
